68ca47d0c0b92463a5112738e1de5097cbe9adfe
SEK1RO <vlad@sek1.ro>
2025-09-02 20:00:20 +0300

    feat(Diagram component): minterm cells

--- src/components/Diagram.vue
+++ src/components/Diagram.vue
@@ -1,41 +1,60 @@
 <script setup>
 const props = defineProps({
+  funLen: Number,
   rectW: Number,
   rectH: Number,
   rects: Array,
   paths: Array,
   texts: Array,
   variables: Array,
+  t1: Array,
+  tx: Array,
+  mt: Array,
 })
+
+function isMinterm(idx) {
+  let bin = idx.toString(2)
+  while (bin.length != props.funLen) {
+    bin = '0' + bin
+  }
+  for (const mt of props.mt) {
+    const mt_sliced = mt.slice(0, mt.length - 1)
+    const mt_regex = mt_sliced.replaceAll('*', '.')
+    if (bin.match(mt_regex)) {
+      console.log(bin, mt_regex)
+      return true
+    }
+  }
+}
 </script>
 
 <template>
   <svg viewBox="-30 -30 170 170" xmlns="http://www.w3.org/2000/svg">
-  <template v-for="rect, idx in rects" :key="idx">
-    <rect :x="rect.x" :y="rect.y" :width="rectW" :height="rectH"/>
+  <template v-for="rect, idx in props.rects" :key="idx">
+    <rect :class="isMinterm(idx) ? 'minterm' : ''" :x="rect.x" :y="rect.y" :width="props.rectW" :height="props.rectH"/>
     <text class="t1"
-      v-if="route.query.t1?.find(t => t == idx)"
-      :x="rect.x + rectW * 0.5"
-      :y="rect.y + rectH * 0.65"
+      v-if="props.t1.find(t => t == idx)"
+      :x="rect.x + props.rectW * 0.5"
+      :y="rect.y + props.rectH * 0.65"
     >1</text>
     <text class="tx"
-      v-else-if="route.query.tx?.find(t => t == idx)"
-      :x="rect.x + rectW * 0.5"
-      :y="rect.y + rectH * 0.65"
+      v-else-if="props.tx.find(t => t == idx)"
+      :x="rect.x + props.rectW * 0.5"
+      :y="rect.y + props.rectH * 0.65"
     >x</text>
     <text class="t0"
       v-else
-      :x="rect.x + rectW * 0.5"
-      :y="rect.y + rectH * 0.65"
+      :x="rect.x + props.rectW * 0.5"
+      :y="rect.y + props.rectH * 0.65"
     >0</text>
-    <text class="numbers" :x="rect.x + rectW * 0.95" :y="rect.y + rectH * 0.95">{{ idx }}</text>
+    <text class="numbers" :x="rect.x + props.rectW * 0.95" :y="rect.y + props.rectH * 0.95">{{ idx }}</text>
   </template>
   <path
-    v-for="path, idx in paths" :key="idx"
+    v-for="path, idx in props.paths" :key="idx"
     :transform="path.t"
     :stroke-width="path.sw"
     d="M0 50c-3 0 -6 -3 -6 -6v-13c0 -3 -3 -6 -6 -6c3 0 6 -3 6 -6v-13c0 -3 3 -6 6 -6"/>
-  <text class="variables" v-for="text, idx in texts" :key="idx" :x="text.x" :y="text.y">{{ variables[idx] }}</text>
+  <text class="variables" v-for="text, idx in props.texts" :key="idx" :x="text.x" :y="text.y">{{ props.variables[idx] }}</text>
 </svg>
 </template>
 
@@ -77,5 +96,9 @@ svg .numbers {
 svg .variables {
   font-size: 12px;
   text-anchor: middle;
+}
+
+svg .minterm {
+  fill: rgb(225, 225, 225);
 }
 </style>
--- src/views/VeitchView.vue
+++ src/views/VeitchView.vue
@@ -2,7 +2,7 @@
 import { computed, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRoute } from 'vue-router';
-// import Diagram from '@/components/Diagram.vue';
+import Diagram from '@/components/Diagram.vue';
 
 const { t } = useI18n()
 const route = useRoute()
@@ -93,35 +93,25 @@ switch (route.query.funLen) {
     </h3>
     <div v-else class="diagrams">
       <h3 class="text-mono">{{ t('fun_minimizer.text_mdnf') }}</h3>
-      <svg id="svg-MDNF" viewBox="-30 -30 170 170" xmlns="http://www.w3.org/2000/svg">
-        <template v-for="rect, idx in rects" :key="idx">
-          <rect :x="rect.x" :y="rect.y" :width="rectW" :height="rectH"/>
-          <text class="t1"
-            v-if="route.query.t1?.find(t => t == idx)"
-            :x="rect.x + rectW * 0.5"
-            :y="rect.y + rectH * 0.65"
-          >1</text>
-          <text class="tx"
-            v-else-if="route.query.tx?.find(t => t == idx)"
-            :x="rect.x + rectW * 0.5"
-            :y="rect.y + rectH * 0.65"
-          >x</text>
-          <text class="t0"
-            v-else
-            :x="rect.x + rectW * 0.5"
-            :y="rect.y + rectH * 0.65"
-          >0</text>
-          <text class="numbers" :x="rect.x + rectW * 0.95" :y="rect.y + rectH * 0.95">{{ idx }}</text>
-        </template>
-        <path
-          v-for="path, idx in paths" :key="idx"
-          :transform="path.t"
-          :stroke-width="path.sw"
-          d="M0 50c-3 0 -6 -3 -6 -6v-13c0 -3 -3 -6 -6 -6c3 0 6 -3 6 -6v-13c0 -3 3 -6 6 -6"/>
-        <text class="variables" v-for="text, idx in texts" :key="idx" :x="text.x" :y="text.y">{{ variables[idx] }}</text>
-      </svg>
+      <diagram
+        :fun-len="route.query.funLen"
+        :rect-w="rectW"
+        :rect-h="rectH"
+        :rects :paths
+        :texts :variables
+        :t1="route.query.t1"
+        :tx="route.query.tx"
+        :mt="route.query.MDNF"/>
       <h3 class="text-mono">{{ t('fun_minimizer.text_mcnf') }}</h3>
-      <!-- <diagram :rect-w="rectW" :rect-h="rectH" :rects :paths :texts :variables/> -->
+      <diagram
+        :fun-len="route.query.funLen"
+        :rect-w="rectW"
+        :rect-h="rectH"
+        :rects :paths
+        :texts :variables
+        :t1="route.query.t1"
+        :tx="route.query.tx"
+        :mt="route.query.MKNF"/>
     </div>
   </div>
 </template>
@@ -129,45 +119,6 @@ switch (route.query.funLen) {
 <style scoped lang="scss">
 h3 {
   font-size: 28px;
-}
-
-svg {
-  width: 500px;
-  fill: none;
-  stroke: #2c3e50;
-  stroke-width: 1px;
-  stroke-linejoin: round;
-  stroke-linecap: round;
-}
-
-svg text {
-  fill: #2c3e50;
-  stroke-width: 0;
-}
-
-svg .t1 {
-  fill: rgb(40, 192, 40);
-}
-
-svg .t0 {
-  fill: red;
-}
-
-svg .t1,
-svg .t0,
-svg .tx {
-  font-size: 12px;
-  text-anchor: middle;
-}
-
-svg .numbers {
-  font-size: 6px;
-  text-anchor: end;
-}
-
-svg .variables {
-  font-size: 12px;
-  text-anchor: middle;
 }
 
 .wrapper {
